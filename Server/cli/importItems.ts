149135e894b0c208254add5cbbe47d65c53d5e22
Meldiron <matejbacocom@gmail.com>
2019-10-13 18:40:02 +0200

    added BE items

--- Server/cli/importItems.ts
+++ Server/cli/importItems.ts
@@ -1,13 +1,30 @@
-const items = [
-  {
-    tag: 'knife',
-    correctAnswer: 'Blue',
-  },
-];
+const items = [];
 
 var fs = require('fs');
 var files = fs.readdirSync(
-  '/Users/meldiron/Desktop/recycle-game-unity/Server/build',
+  '/Users/meldiron/Desktop/recycle-game-unity/Assets/SimpleItems/Prefabs',
 );
 
-console.log(files);
+let filesArr = files.filter(i => {
+  return !i.includes('.meta');
+});
+
+filesArr = filesArr.map(i => {
+  i = i
+    .split('.prefab')
+    .join('')
+    .split('Items_')
+    .join('')
+    .toLowerCase();
+
+  return i;
+});
+
+filesArr.forEach(i => {
+  items.push({
+    tag: i,
+    correctAnswer: 'SET_ME',
+  });
+});
+
+console.log(JSON.stringify(items));
